8521d9d8bd6038cf7f33c78efa79af4f4b98fbd5
Francisco Barbosa <franciscolamares@gmail.com>
2017-09-08 19:23:32 +0100

    homepage categories dropdown

--- app/src/main/java/com/forgetgot/selftie/Homepage.java
+++ app/src/main/java/com/forgetgot/selftie/Homepage.java
@@ -4,16 +4,36 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+
+import static com.forgetgot.selftie.R.id.mainSpinner;
 import static com.forgetgot.selftie.R.layout.activity_create_task;
 
 public class Homepage extends AppCompatActivity {
+    ArrayList<Float> categories;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+// Array of choices
+        String colors[] = {"Cat1","Cat2","Cat3","Cat3"};
+
+// Selection of the spinner
+        Spinner spinner = (Spinner) findViewById(R.id.mainSpinner);
+
+// Application of the Array to the Spinner
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this,   android.R.layout.simple_spinner_item, colors);
+        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
+        spinner.setAdapter(spinnerArrayAdapter);
     }
+
 
     public void goCreateTask(View v){
         Intent myIntent = new Intent(this, CreateTask.class);
