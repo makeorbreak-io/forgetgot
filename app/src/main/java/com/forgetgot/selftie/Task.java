e3d7a08581cfa92782bce13ef6ce2665411f7052
Francisco Barbosa <franciscolamares@gmail.com>
2017-09-08 18:46:16 +0100

    homepage buttons redirecting

--- app/src/main/java/com/forgetgot/selftie/Task.java
+++ app/src/main/java/com/forgetgot/selftie/Task.java
@@ -1,13 +1,21 @@
 package com.forgetgot.selftie;
 
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.ImageButton;
+
 import java.io.Serializable;
 import java.util.ArrayList;
+
+import static com.forgetgot.selftie.R.id.button;
+import static com.forgetgot.selftie.R.styleable.View;
 
 public class Task implements Serializable{
     static public String TASK_EXTRA = "com.forgetgot.selftie.TASK_EXTRA";
     String name;
     String category;
     double prediction;
+    int Id;
     ArrayList<Double> times;
 
     public Task(String name, String category, double prediction) {
