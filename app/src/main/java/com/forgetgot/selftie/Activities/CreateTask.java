dc119e53978e741325bf0f6ac72fe9e1566c3f0e
inesfproenca <inesfproenca@gmail.com>
2017-09-09 16:44:23 +0100

    Add Task form validation

--- app/src/main/java/com/forgetgot/selftie/Activities/CreateTask.java
+++ app/src/main/java/com/forgetgot/selftie/Activities/CreateTask.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.forgetgot.selftie.Database.DatabaseHandler;
 import com.forgetgot.selftie.Database.Task;
@@ -21,6 +22,13 @@ public class CreateTask extends AppCompatActivity {
     public void createTask(View v){
         String name = ((EditText)findViewById(R.id.taskNameInput)).getText().toString();
         String category = ((EditText)findViewById(R.id.taskCategoryInput)).getText().toString();
+
+        //Form validation
+        if(name.isEmpty() || category.isEmpty() || ((EditText)findViewById(R.id.taskPredictionInput)).getText().toString().isEmpty()){
+            Toast.makeText(this,getString(R.string.required_fields), Toast.LENGTH_LONG).show();
+            return;
+        }
+
         double predictionTime = Double.parseDouble(((EditText)findViewById(R.id.taskPredictionInput)).getText().toString());
 
         Task task = new Task(-1, name, category, predictionTime);
